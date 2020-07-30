a01c6fb65db80d4636641705f540029f84abe353
Smarthard <th3smartchan@gmail.com>
2020-07-30 13:10:41 +0300

    sessions storage fix

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,9 @@
 // TODO: clean up
 
-/* CONFIG CONSTANTS */
-import {ServerError} from "./types/ServerError";
-import {Request, Response} from 'express';
-
-const {SESSION_SECRET, PRODUCTION} = require('./config/auth');
-
 /* EXTERNAL IMPORTS */
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import connectPg from 'connect-pg-simple';
-import express from 'express';
+import express, {Request, Response} from 'express';
 import session from 'express-session';
 import helmet from 'helmet';
 import http from 'http';
@@ -18,14 +11,17 @@ import morgan from 'morgan';
 
 /* LOCAL IMPORTS */
 import {sequelize} from './sequelize';
-import middleware = require('./auth/middleware');
-
-/* SERVER APP CONSTANTS */
-const app = express();
-const PGStore = connectPg(session);
+import {ServerError} from './types/ServerError';
+import * as middleware from './auth/middleware';
 
 /* ROUTES */
 import {users} from './routes/users';
+
+/* SERVER APP CONSTANTS */
+const app = express();
+const {SESSION_SECRET, PRODUCTION} = require('./config/auth');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sessionStorage = new SequelizeStore({ db: sequelize, tableName: 'Sessions' });
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -43,11 +39,7 @@ app.use(session({
         sameSite: 'lax',
         expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // expires in 24h
     },
-    store: new PGStore({
-        tableName: 'Sessions',
-        // TODO: conString refactor?
-        conString: process.env.DATABASE_URL || 'pg://postgres@127.0.0.1:5432/shikicinema',
-    }),
+    store: sessionStorage,
 }));
 app.use(cors());
 app.use(helmet({
@@ -70,10 +62,13 @@ app.use((err: ServerError, req: Request, res: Response, next: Function) => {
 
 /* SERVER STARTUP */
 (async () => {
-    await sequelize
-        .sync({ force: false })
-        .catch(() => console.debug('cannot connect to database'));
+    try {
+        await sessionStorage.sync();
+        await sequelize.authenticate();
 
-    http.createServer(app)
-        .listen(process.env.HTTP_PORT || '3001')
+        http.createServer(app)
+            .listen(process.env.HTTP_PORT || '3001');
+    } catch (err) {
+        console.error(err);
+    }
 })();
