232381bf5021c8f810961c3b6ef2d4224fde8126
Smarthard <th3smartchan@gmail.com>
2020-12-22 15:30:20 +0300

    [no-issue] fix database config env variables

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,25 +3,27 @@ import EntitiesConfig from './entities.config';
 import { UserSeed21607357974819 } from '../../seeds/1607357974819-user-seed';
 import { SessionSeed1607704017485 } from '../../seeds/1607704017485-session-seed';
 
-const postgresConfig = {
-    type:         process.env.SHIKICINEMA_DB_TYPE || 'postgres',
-    host:         process.env.SHIKICINEMA_DB_HOST || 'localhost',
-    port:         process.env.SHIKICINEMA_DB_PORT || 5432,
-    database:     process.env.SHIKICINEMA_DB_NAME || 'shikicinema-dev',
-    username:     process.env.SHIKICINEMA_DB_USER || 'postgres',
-    password:     process.env.SHIKICINEMA_DB_PASS || 'postgres',
-    logging:      process.env.SHIKICINEMA_DB_LOG || false,
-    synchronize:  process.env.SHIKICINEMA_DB_SYNC || false,
-    entities:     EntitiesConfig,
-};
+export default registerAs('database', () => {
+    const postgresConfig = {
+        type:         process.env.SHIKICINEMA_DB_TYPE || 'postgres',
+        host:         process.env.SHIKICINEMA_DB_HOST || 'localhost',
+        port:         process.env.SHIKICINEMA_DB_PORT || 5432,
+        database:     process.env.SHIKICINEMA_DB_NAME || 'shikicinema-dev',
+        username:     process.env.SHIKICINEMA_DB_USER || 'postgres',
+        password:     process.env.SHIKICINEMA_DB_PASS || 'postgres',
+        logging:      process.env.SHIKICINEMA_DB_LOG || false,
+        synchronize:  process.env.SHIKICINEMA_DB_SYNC || false,
+        entities:     EntitiesConfig,
+    };
 
-const inMemoryConfig = {
-    type: 'sqlite',
-    database: ':memory:',
-    entities: EntitiesConfig,
-    logging: false,
-    synchronize: true,
-    migrations: [ UserSeed21607357974819, SessionSeed1607704017485 ]
-};
+    const inMemoryConfig = {
+        type: 'sqlite',
+        database: ':memory:',
+        entities: EntitiesConfig,
+        logging: false,
+        synchronize: true,
+        migrations: [ UserSeed21607357974819, SessionSeed1607704017485 ]
+    };
 
-export default registerAs('database', () => process.env.NODE_ENV === 'testing' ? inMemoryConfig : postgresConfig);
+    return process.env.NODE_ENV === 'testing' ? inMemoryConfig : postgresConfig
+});
