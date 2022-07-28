7c65d7ed51d7c37e45e2b211af708f6ac4f9bc6f
Smarthard <th3smartchan@gmail.com>
2022-07-28 11:18:24 +0300

    [#2][CR-2] rename file dto to match class name

--- src/routes/api/video/dto/index.ts
+++ src/routes/api/video/dto/index.ts
@@ -3,5 +3,5 @@ export { GetVideosRequest } from '@app-routes/api/video/dto/GetVideosRequest.dto
 export { VideoResponse } from '@app-routes/api/video/dto/VideoResponse.dto';
 export { GetVideosResponse } from '@app-routes/api/video/dto/GetVideosResponse.dto';
 export { AnimeEpisodeInfo } from '@app-routes/api/video/dto/AnimeEpisodeInfo.dto';
-export { SearchVideosRequest } from '@app-routes/api/video/dto/GetVideosByUploaderRequest.dto';
+export { SearchVideosRequest } from '@app-routes/api/video/dto/SearchVideosRequest.dto';
 export { GetVideosInfoRequest } from '@app-routes/api/video/dto/GetVideosInfoRequest.dto';
