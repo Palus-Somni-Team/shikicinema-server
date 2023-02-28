bb106964e581dbc93084ca5452a530958af2958c
Smarthard <th3smartchan@gmail.com>
2023-02-28 18:13:43 +0300

    [#92] exclude swagger documentation for oauth controller

--- src/routes/oauth/oauth-with-authorization.controller.ts
+++ src/routes/oauth/oauth-with-authorization.controller.ts
@@ -1,3 +1,4 @@
+import { ApiExcludeController } from '@nestjs/swagger';
 import { AuthenticatedGuard } from '@app-guards/authenticated.guard';
 import { OAuthController } from 'nestjs-oauth2';
 import { ShikimoriTransformInterceptor } from '@app-routes/oauth/interceptors/shikimori-transform.interceptor';
@@ -5,4 +6,5 @@ import { UseGuards, UseInterceptors } from '@nestjs/common';
 
 @UseGuards(AuthenticatedGuard)
 @UseInterceptors(ShikimoriTransformInterceptor)
+@ApiExcludeController()
 export class OAuthWithAuthorizationController extends OAuthController {}
